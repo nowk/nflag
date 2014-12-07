8f31120aabe06e1f26d1c777dd120e586163ddbe
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2014-12-06 21:20:44 -0500

    Remove Var

--- flag.go
+++ flag.go
@@ -6,35 +6,6 @@ import (
 	"time"
 )
 
-// Var type checks the default value to call the proper <T>Var function
-func Var(p interface{}, n, fl string, d interface{}, usage string) {
-	switch d.(type) {
-	case bool:
-		BoolVar(p.(*bool), n, fl, d.(bool), usage)
-
-	case time.Duration:
-		DurationVar(p.(*time.Duration), n, fl, d.(time.Duration), usage)
-
-	case float64:
-		Float64Var(p.(*float64), n, fl, d.(float64), usage)
-
-	case int:
-		IntVar(p.(*int), n, fl, d.(int), usage)
-
-	case int64:
-		Int64Var(p.(*int64), n, fl, d.(int64), usage)
-
-	case string:
-		StringVar(p.(*string), n, fl, d.(string), usage)
-
-	case uint:
-		UintVar(p.(*uint), n, fl, d.(uint), usage)
-
-	case uint64:
-		Uint64Var(p.(*uint64), n, fl, d.(uint64), usage)
-	}
-}
-
 // def looks up env and returns that or the default depending on whether it was
 // set
 func def(n string, d interface{}) interface{} {
